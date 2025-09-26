2379f6696a266b0bb96bd54ba3f8241c81432f01
RAFAEL ELIAS PEREZ PEREZ <repp2208@gmail.com>
2025-09-26 13:24:31 -0600

    Revert "DISEÑO"
    This reverts commit 48846151604a171c924853e33ae5e73824de84e6.

--- presentacion/src/app/app.routes.ts
+++ presentacion/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Documentador } from './documentador/documentador';
+import { Tester } from './tester/tester';
 
 export const routes: Routes = [
 	{ path: '', component: Home },
 	{ path: 'documentador', component: Documentador },
+    { path: 'tester', component: Tester },
 ];
--- presentacion/src/app/documentador/documentador.ts
+++ presentacion/src/app/documentador/documentador.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-documentador',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './documentador.html',
   styleUrl: './documentador.css'
 })
