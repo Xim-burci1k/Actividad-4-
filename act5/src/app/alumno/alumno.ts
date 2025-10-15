e8ae4e11c308d65f9b94212153919e9ce92530de
XIMENA BURCIAGA PEREZ <almn.ximenaburciaga@cbtis66.edu.mx>
2025-10-15 14:43:59 -0600

    HOLAAAAAAAAAAAAAAAAA
    BYE

---
+++ act5/src/app/alumno/alumno.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {router} from '@ angular/router';
+
+@Component({
+  selector: 'app-alumno',
+  imports: [],
+  templateUrl: './alumno.html',
+  styleUrl: './alumno.css'
+})
+export class Alumno {
+  consructor() {}
+  ngOnInit(): void {}
+  onAddAlumno() {}
+  onModificarAlumno() {}
+  onEliminarAlmno() {}
+}
